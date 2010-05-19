88410672f3986aad962bd75022cef4f9979e1a79
James Logsdon <dwarf@girsbrain.org>
2010-05-19 15:33:11 -0700

    Don't need the list of colors anymore, but the reference is handy.

--- lib/cli/Colors.php
+++ lib/cli/Colors.php
@@ -15,34 +15,7 @@ namespace cli;
 /**
  * Change the color of text.
  *
- * 0	Reset all attributes
- * 1	Bright
- * 2	Dim
- * 4	Underscore	
- * 5	Blink
- * 7	Reverse
- * 8	Hidden
- *
- *	Foreground Colors
- *	30	Black
- *	31	Red
- *	32	Green
- *	33	Yellow
- *	34	Blue
- *	35	Magenta
- *	36	Cyan
- *	37	White
- *
- *		Background Colors
- *		40	Black
- *		41	Red
- *		42	Green
- *		43	Yellow
- *		44	Blue
- *		45	Magenta
- *		46	Cyan
- *		47	White
- *
+ * Reference: http://graphcomp.com/info/specs/ansi_col.html#colors
  */
 class Colors {
 	static protected $_styles = array(
