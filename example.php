9b6c3a5b6d6aa4a627d706a51dd1da5782626b78
James Logsdon <dwarf@girsbrain.org>
2010-05-20 16:10:52 +0000

    Forcing error reporting on the example script

--- example.php
+++ example.php
@@ -4,6 +4,11 @@ if (php_sapi_name() != 'cli') {
 	die('Must run from command line');
 }
 
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', 1);
+ini_set('log_errors', 0);
+ini_set('html_errors', 0);
+
 require 'lib/cli/cli.php';
 \cli\register_autoload();
 
