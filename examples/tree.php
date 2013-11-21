d2709b83e8f0695bb08560bffd87d1362ccab59b
Ryan <rsullivan@connectstudios.com>
2013-11-21 11:40:35 -0500

    Add example for ASCII

--- examples/tree.php
+++ examples/tree.php
@@ -23,8 +23,34 @@ $data = [
     ],
 ];
 
+printf("ASCII:\n");
+
 /**
- * Markdown
+ * ASCII should look something like this:
+ *
+ * -Test
+ * |\-Something Cool
+ * ||\-This is a 3rd layer
+ * |\-This is a 2nd layer
+ * \-Other test
+ *  \-This is awesome
+ *   \-This is also cool
+ *   \-This is even cooler
+ *   \-Wow like what is this
+ *    \-Awesome eh?
+ *    \-Totally
+ *     \-Yep!
+ */
+
+$tree = new \cli\Tree;
+$tree->setData($data);
+$tree->setRenderer(new \cli\tree\Ascii);
+$tree->display();
+
+printf("\nMarkdown:\n");
+
+/**
+ * Markdown looks like this:
  *
  * - Test
  *     - Something Cool
