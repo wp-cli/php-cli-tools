1e46d972d18f90cd9c485da31a95d9e1cddb3935
Ryan <rsullivan@connectstudios.com>
2013-11-21 11:33:51 -0500

    Add Tree example and fix common to use correct autoloader

---
+++ examples/tree.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once 'common.php';
+
+$data = [
+    'Test' => [
+        'Something Cool' => [
+            'This is a 3rd layer',
+        ],
+        'This is a 2nd layer',
+    ],
+    'Other test' => [
+        'This is awesome' => [
+            'This is also cool',
+            'This is even cooler',
+            'Wow like what is this' => [
+                'Awesome eh?',
+                'Totally' => [
+                    'Yep!'
+                ],
+            ],
+        ],
+    ],
+];
+
+/**
+ * Markdown
+ *
+ * - Test
+ *     - Something Cool
+ *         - This is a 3rd layer
+ *     - This is a 2nd layer
+ * - Other test
+ *     - This is awesome
+ *         - This is also cool
+ *         - This is even cooler
+ *         - Wow like what is this
+ *             - Awesome eh?
+ *             - Totally
+ *                 - Yep!
+ */
+
+$tree = new \cli\Tree;
+$tree->setData($data);
+$tree->setRenderer(new \cli\tree\Markdown(4));
+$tree->display();
+
