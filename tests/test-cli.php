a3b36d38930d60f51b1e390e32c8ab2270778697
Zack Tollman <tollmanz@gmail.com>
2014-08-03 18:47:17 -0700

    Add tests for colorization and decolorization.

--- tests/test-cli.php
+++ tests/test-cli.php
@@ -20,4 +20,28 @@ class testsCli extends PHPUnit_Framework_TestCase {
 		$this->assertEquals( \cli\Colors::length( \cli\Colors::colorize( '%Gx%n', true ) ), 1 );
 	}
 
+	function test_colorize_string_is_colored() {
+		$original = '%Gx';
+		$colorized = "\033[32;1mx";
+
+		$this->assertEquals( \cli\Colors::colorize( $original, true ), $colorized );
+	}
+
+	function test_colorize_when_colorize_is_forced() {
+		$original = '%gx%n';
+
+		$this->assertEquals( \cli\Colors::colorize( $original, false ), 'x' );
+	}
+
+	function test_binary_string_is_converted_back_to_original_string() {
+		$string            = 'x';
+		$string_with_color = '%b' . $string;
+		$colorized_string  = "\033[34m$string";
+
+		// Ensure colorization is applied correctly
+		$this->assertEquals( \cli\Colors::colorize( $string_with_color, true ), $colorized_string );
+
+		// Ensure that the colorization is reverted
+		$this->assertEquals( \cli\Colors::decolorize( $colorized_string ), $string );
+	}
 }
