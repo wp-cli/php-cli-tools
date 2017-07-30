65b5ac2612ac6f6741634a9d54f34a7ba9975fbb
gitlost <burmartke@gmail.com>
2017-07-30 23:14:43 +0100

    Fix reverse (inverse) and white (grey).

---
+++ tests/test-colors.php
@@ -0,0 +1,27 @@
+<?php
+
+use cli\Colors;
+
+class testsColors extends PHPUnit_Framework_TestCase {
+
+	/**
+     * @dataProvider dataColors
+	 */ 
+	function testColors( $str, $color ) {
+		$colored = Colors::color( $color );
+		$this->assertSame( Colors::colorize( $str ), Colors::color( $color ) );
+		if ( in_array( 'reset', $color ) ) {
+			$this->assertTrue( false !== strpos( $colored, '[0m' ) );
+		} else {
+			$this->assertTrue( false === strpos( $colored, '[0m' ) );
+		}
+	}
+
+	function dataColors() {
+		$ret = array();
+		foreach ( Colors::getColors() as $str => $color ) {
+			$ret[] = array( $str, $color );
+		}
+		return $ret;
+	}
+}
