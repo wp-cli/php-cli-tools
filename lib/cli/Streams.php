7834a80390aa317a8aaa53699ecfb91de3c8ea64
Valeriy Seregin <valera-s-38@mail.ru>
2021-01-12 15:38:03 +0300

    Fix type hinting for prompt function

--- lib/cli/Streams.php
+++ lib/cli/Streams.php
@@ -156,7 +156,7 @@ class Streams {
 	 * @return string  The users input.
 	 * @see cli\input()
 	 */
-	public static function prompt( $question, $default = null, $marker = ': ', $hide = false ) {
+	public static function prompt( $question, $default = false, $marker = ': ', $hide = false ) {
 		if( $default && strpos( $question, '[' ) === false ) {
 			$question .= ' [' . $default . ']';
 		}
