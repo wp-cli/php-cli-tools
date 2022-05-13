36a124c2e908255031e042fd140f967f709bea00
Guillaume Seznec <767901+aerogus@users.noreply.github.com>
2022-05-13 17:29:14 +0200

    hide php8.1 deprecated messages
    ex: Deprecated: Return type of cli\Arguments::offsetSet($offset, $value) should either be compatible with ArrayAccess::offsetSet(mixed $offset, mixed $value): void, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice

--- lib/cli/Arguments.php
+++ lib/cli/Arguments.php
@@ -86,6 +86,7 @@ class Arguments implements \ArrayAccess {
 	 * @param mixed  $offset  An Argument object or the name of the argument.
 	 * @return bool
 	 */
+	#[\ReturnTypeWillChange]
 	public function offsetExists($offset) {
 		if ($offset instanceOf Argument) {
 			$offset = $offset->key;
@@ -100,6 +101,7 @@ class Arguments implements \ArrayAccess {
 	 * @param mixed  $offset  An Argument object or the name of the argument.
 	 * @return mixed
 	 */
+	#[\ReturnTypeWillChange]
 	public function offsetGet($offset) {
 		if ($offset instanceOf Argument) {
 			$offset = $offset->key;
@@ -116,6 +118,7 @@ class Arguments implements \ArrayAccess {
 	 * @param mixed  $offset  An Argument object or the name of the argument.
 	 * @param mixed  $value   The value to set
 	 */
+	#[\ReturnTypeWillChange]
 	public function offsetSet($offset, $value) {
 		if ($offset instanceOf Argument) {
 			$offset = $offset->key;
@@ -129,6 +132,7 @@ class Arguments implements \ArrayAccess {
 	 *
 	 * @param mixed  $offset  An Argument object or the name of the argument.
 	 */
+	#[\ReturnTypeWillChange]
 	public function offsetUnset($offset) {
 		if ($offset instanceOf Argument) {
 			$offset = $offset->key;
