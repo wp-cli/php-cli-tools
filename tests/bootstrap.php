8a56070621a080ab1dc477fcfca3de6266442566
Pascal Birchler <pascalb@google.com>
2024-05-22 22:00:14 +0200

    Use `strtolower`

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,7 +6,7 @@ function cli_autoload( $className ) {
 	$fileName  = '';
 	$namespace = '';
 	if ($lastNsPos = strrpos($className, '\\')) {
-		$namespace = substr($className, 0, $lastNsPos);
+		$namespace = strtolower(substr($className, 0, $lastNsPos));
 		$className = substr($className, $lastNsPos + 1);
 		$fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
 	}
